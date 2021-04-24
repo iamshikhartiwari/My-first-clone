b7abbbb1b05ac1eb1eb5f402b1ae3867c8cc00f9
Shikhar Tiwari <72856503+iamshikhartiwari@users.noreply.github.com>
2021-04-24 07:59:27 +0530

    Initial Commit

---
+++ practice.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    
+    cout<<"write a no. :"<<n;
+    cin>>n;
+    cout<<"AP is given below";
+    int a=2,d=2;
+    for(int i=a;)
+    return 0;
+}
